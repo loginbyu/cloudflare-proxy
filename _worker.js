c92b6fe82e82a6f8d030212f09d5ae29d7b5f633
loginbyu <loginbyu03@gmail.com>
2026-09-20 08:24:01 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -8,8 +8,8 @@ import { connect } from 'cloudflare:sockets';
 
 let subPath = 'link';     // 节点订阅路径,不修改将使用uuid作为订阅路径
 let password = '123456';  // 主页密码,建议修改或添加 PASSWORD环境变量
-let proxyIP = 'proxy.xxxxxxxx.tk:50001';  // proxyIP 格式：ip、域名、ip:port、域名:port等,没填写port，默认使用443
-let yourUUID = '5dc15e15-f285-4a9d-959b-0e4fbdd77b63'; // UUID,建议修改或添加环境便量
+let proxyIP = '147.139.189.225:443';  // proxyIP 格式：ip、域名、ip:port、域名:port等,没填写port，默认使用443
+let yourUUID = '124799ca-66d8-470b-af7c-1823680ac13c'; // UUID,建议修改或添加环境便量
 let disabletro = false;  // 是否关闭trojan, 设置为true时关闭，false开启 
 
 // CDN 
